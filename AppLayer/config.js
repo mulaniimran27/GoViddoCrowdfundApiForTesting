e7f7ca0a6e30879c3c2d75269beae33a70281a82
GoViddo <contact@goviddo.com>
2019-12-04 23:04:11 +0530

    Demo

--- AppLayer/config.js
+++ AppLayer/config.js
@@ -2,7 +2,7 @@ var goviddo = goviddo || {};
 
 (function (goviddo) {
     goviddo.config = {
-        port: 1996,
+        port: 3000,
         host: 'sql138.main-hosting.eu',
         user: 'u450042391_govid',
         password: 'Imran1234',
